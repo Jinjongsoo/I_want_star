dd645c533b1fbf45559e4f283b3d778377eb491a
habitance <positipman@gmail.com>
2019-04-17 12:42:32 +0900

    feat: add fizzbuzz function finished

--- main.py
+++ main.py
@@ -1,10 +1,15 @@
 # make fizzbuzz code
-for i in range(1, 101):
-    if i % 15 == 0:
-        print('fizzbuzz')
-    elif i % 3 == 0:
-        print('fizz')
-    elif i % 5 == 0:
-        print('buzz')
-    else:
-        print(i)
+def fizzbuzz():
+    for i in range(1, 101):
+        if i % 15 == 0:
+            print('fizzbuzz')
+        elif i % 3 == 0:
+            print('fizz')
+        elif i % 5 == 0:
+            print('buzz')
+        else:
+            print(i)
+
+if __name__ == "__main__":
+    fizzbuzz()
+
